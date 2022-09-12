90b801f4d2b4decaa932e0bc48dd09fc606b91f3
Mercy Ek <me460925@gmail.com>
2022-09-12 02:54:55 -0500

    change2

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 me is mentionedi
-app.get('/', (req, res) => res.send('my congratulations is here amen!'));
+app.get('/', (req, res) => res.send('my congratulations is here amen Lord!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
